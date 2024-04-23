29cb15089364a848b64744fef41e201eb3339835
Tomas H <horychtomas98@gmail.com>
2024-04-23 13:51:30 +0900

    add scripts

---
+++ annomatic-dataset/evaluation/hyperparameter_tuning.py
@@ -0,0 +1,150 @@
+# %% imports
+import logging
+import warnings
+
+import pandas as pd
+import torch
+import transformers
+from datasets import load_dataset, Dataset
+from transformers import (
+    AutoModelForSequenceClassification,
+    AutoTokenizer,
+    DataCollatorWithPadding,
+    EarlyStoppingCallback,
+    Trainer,
+    TrainingArguments,
+)
+
+import wandb
+from evaluation.utils import set_random_seed, compute_metrics_hf
+
+# %% warning surpress
+transformers.logging.set_verbosity(transformers.logging.ERROR)
+logging.disable(logging.ERROR)
+warnings.filterwarnings("ignore", category=UserWarning)
+
+
+# %% definitions
+base_model = "roberta-base"
+magpie = "mediabiasgroup/lbm_without_media_bias_pretrained"
+device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+
+# %% prepare model & data
+anno_lex = load_dataset("mediabiasgroup/anno-lexical")
+
+
+model = AutoModelForSequenceClassification.from_pretrained(base_model, num_labels=2)
+tokenizer = AutoTokenizer.from_pretrained(base_model)
+data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
+
+# %% tokenize data
+
+
+tok = tokenizer(
+    list(anno_lex["train"]["text"]),
+    truncation=True,
+    padding=True,
+    max_length=128,
+    return_tensors="pt",
+)
+anno_lex_train_t = Dataset.from_dict(
+    {
+        "input_ids": tok["input_ids"],
+        "attention_mask": tok["attention_mask"],
+        "label": anno_lex["train"]["label"],
+    }
+)
+
+tok = tokenizer(
+    list(anno_lex["dev"]["text"]),
+    truncation=True,
+    padding=True,
+    max_length=128,
+    return_tensors="pt",
+)
+anno_lex_dev_t = Dataset.from_dict(
+    {
+        "input_ids": tok["input_ids"],
+        "attention_mask": tok["attention_mask"],
+        "label": anno_lex["dev"]["label"],
+    }
+)
+
+tok = tokenizer(
+    list(anno_lex["test"]["text"]),
+    truncation=True,
+    padding=True,
+    max_length=128,
+    return_tensors="pt",
+)
+anno_lex_test_t = Dataset.from_dict(
+    {
+        "input_ids": tok["input_ids"],
+        "attention_mask": tok["attention_mask"],
+        "label": anno_lex["test"]["label"],
+    }
+)
+
+
+def train_wrapper():
+    """Execute the wandb hyperparameter tuning job.
+
+    Takes the (globally defined) tasks, instantiates a trainer for them.
+    This function is passed as a callback to wandb.
+    """
+    wandb.init(entity="media-bias-group", project="annomatic_dataset_hyperparams")
+    set_random_seed()
+    sweep_model = AutoModelForSequenceClassification.from_pretrained(base_model, num_labels=2)
+
+    training_args = TrainingArguments(
+        report_to="wandb",
+        output_dir="./checkpoints",
+        per_device_eval_batch_size=64,
+        per_device_train_batch_size=32,
+        num_train_epochs=wandb.config.epoch,
+        evaluation_strategy="steps",
+        logging_steps=25,
+        eval_steps=25,
+        disable_tqdm=False,
+        weight_decay=wandb.config.weight_decay,
+        warmup_ratio=0.1,
+        learning_rate=wandb.config.lr,
+        run_name="hyperparam-tuning",
+        remove_unused_columns=False,
+    )
+
+    trainer = Trainer(
+        sweep_model,
+        training_args,
+        train_dataset=anno_lex_train_t,
+        eval_dataset=anno_lex_dev_t,
+        tokenizer=tokenizer,
+        compute_metrics=compute_metrics_hf,
+        data_collator=DataCollatorWithPadding(tokenizer=tokenizer),
+    )
+
+    trainer.train()
+
+
+# wandb sweep
+
+# first search
+# hyper_param_dict = {
+#     "epoch": {"values": [3,5,10]},
+#     "lr": {"values": [2e-5, 3e-5, 4e-5, 1e-4]},
+#     "weight_decay": {"values": [0.01,0.05,0.1]},
+# }
+    
+# second search
+hyper_param_dict = {
+    "epoch": {"values": [3]},
+    "lr": {"values": [1e-5,2e-5,3e-5,5e-6,1e-6]},
+    "weight_decay": {"values": [0.05]},
+}
+
+sweep_config = {"method": "grid"}
+sweep_config["parameters"] = hyper_param_dict
+sweep_config["name"] = "hyperparam-tuning"
+sweep_id = wandb.sweep(sweep_config, project="annomatic_dataset_hyperparams")
+
+wandb.agent(sweep_id, train_wrapper)
---
+++ annomatic-dataset/evaluation/prepare_data.py
@@ -0,0 +1,60 @@
+# %%
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+
+# %%
+df = pd.read_parquet("../data/output/anno-lexical.parquet")
+
+# %%
+df = df[["text", "source_party", "source_name","final_label","sentence_id"]]
+df = df.rename(columns={"final_label": "label"})
+
+# %%
+train_df, devtest_df = train_test_split(df, test_size=0.3, stratify=df.source_party,random_state=42)
+dev_df, test_df = train_test_split(
+    devtest_df, test_size=0.5, stratify=devtest_df.source_party,random_state=42
+)
+
+
+# %%
+train_df.to_parquet('../data/training/anno-lexical-train.parquet',index=False)
+dev_df.to_parquet('../data/training/anno-lexical-dev.parquet',index=False)
+test_df.to_parquet('../data/training/anno-lexical-test.parquet',index=False)
+
+train_df.to_csv('../data/training/anno-lexical-train.csv',index=False)
+dev_df.to_csv('../data/training/anno-lexical-dev.csv',index=False)
+test_df.to_csv('../data/training/anno-lexical-test.csv',index=False)
+
+
+# %%
+print("Label distributions")
+print(train_df.label.value_counts(normalize=True))
+print(dev_df.label.value_counts(normalize=True))
+print(test_df.label.value_counts(normalize=True))
+
+print("Party distributions")
+print(train_df.source_party.value_counts(normalize=True))
+print(dev_df.source_party.value_counts(normalize=True))
+print(test_df.source_party.value_counts(normalize=True))
+# %%
+print("Per party label distribution")
+
+print("\nLeft")
+print(train_df[train_df['source_party']=='Left'].label.value_counts(normalize=True))
+print(dev_df[dev_df['source_party']=='Left'].label.value_counts(normalize=True))
+print(test_df[test_df['source_party']=='Left'].label.value_counts(normalize=True))
+
+print("\nRight")
+print(train_df[train_df['source_party']=='Right'].label.value_counts(normalize=True))
+print(dev_df[dev_df['source_party']=='Right'].label.value_counts(normalize=True))
+print(test_df[test_df['source_party']=='Right'].label.value_counts(normalize=True))
+
+# %%
+print("Per source label distribution")
+
+print("\nLeft")
+print(train_df[train_df['source_name']=='alternet'].label.value_counts(normalize=True))
+print(dev_df[dev_df['source_name']=='alternet'].label.value_counts(normalize=True))
+print(test_df[test_df['source_name']=='alternet'].label.value_counts(normalize=True))
+# %%
