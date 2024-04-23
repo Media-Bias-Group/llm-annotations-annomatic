396981862cdd1b0cf826e275e58188a5f3669023
Tomas H <horychtomas98@gmail.com>
2024-04-23 17:12:46 +0900

    removed annomatic from annomatic-sentence-pool

--- annomatic-sentence-pool/src/transform/transform_data.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-python transform/filter_articles.py
-python transform/create_sentences.py
-python transform/filter_sentences.py
-python transform/create_final_dataset.py
---
+++ evaluation/config.py
@@ -0,0 +1,12 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv(dotenv_path="local.env")
+# load_dotenv()
+
+WANDB_KEY = os.getenv("WANDB_KEY")
+HF_TOKEN = os.getenv("HF_TOKEN")
+PROJECT_NAME = "annomatic_dataset"
+BATCH_SIZE = 32
+RANDOM_SEED = 321
