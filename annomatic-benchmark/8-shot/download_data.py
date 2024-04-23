609213e7e8706e8a173b36f541068d896ab7b9d0
Tomas H <horychtomas98@gmail.com>
2024-04-23 13:24:09 +0900

    added scripts for downloading data from wandb

---
+++ annomatic-benchmark/8-shot/download_data.py
@@ -0,0 +1,30 @@
+from dotenv import load_dotenv
+import wandb
+import os
+
+from tqdm import tqdm
+
+if __name__ == '__main__':
+    # Load environment variables from the .env file
+    load_dotenv()
+    wandb.login(key=os.environ['WANDB'])
+
+    # Specify the project name and tag
+    project_name = 'media-bias-group/annomatic_benchmark'
+    tag = '8-shot'
+
+    # Get the runs
+    runs = wandb.Api().runs(project_name, filters={"tags": tag})
+
+    for run in tqdm(runs):
+        # split the model name to get the output name
+        parts = run.config['model'].split('/')
+        if len(parts) == 2:
+            csv_name = parts[1] + ".csv"
+        else:
+            csv_name = parts[0] + ".csv"
+
+        # download the csv file
+        if csv_name in [x.name for x in run.files()]:
+            csv_file = [x for x in run.files() if x.name == csv_name][0]
+            csv_file.download(root='./data/', replace=True)
