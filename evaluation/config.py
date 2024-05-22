4fcf226527e8a4f899142e512610116542874ca9
horychtom <horychtomas98@gmail.com>
2024-05-22 09:53:20 +0200

    debvugging cluster

--- evaluation/config.py
+++ evaluation/config.py
@@ -7,6 +7,6 @@ load_dotenv(dotenv_path="local.env")
 
 WANDB_KEY = os.getenv("WANDB_KEY")
 HF_TOKEN = os.getenv("HF_TOKEN")
-PROJECT_NAME = "annomatic_dataset"
+PROJECT_NAME = "training-new-babe"
 BATCH_SIZE = 32
 RANDOM_SEED = 321
