009005687c91b5013ed323ce51897116e0d569df
Tomas H <horychtomas98@gmail.com>
2024-05-23 11:46:36 +0900

    basil adapt

--- spurious_cues/get_feature_attributions.py
+++ spurious_cues/get_feature_attributions.py
@@ -6,17 +6,18 @@ babe_base = "mediabiasgroup/babe-base-annomatic"
 magpie = "mediabiasgroup/magpie-annomatic"
 synth = "mediabiasgroup/roberta-anno-lexical-ft"
 
-dataset = "mediabiasgroup/BABE"
+# dataset = "mediabiasgroup/BABE"
+dataset = "horychtom/experiments"
 
 
 # explain feature attributions
-ann = LimeExplainer(babe_base, dataset,split='test')
+ann = LimeExplainer(babe_base, dataset,split='train')
 ann.explain_dataset(class_=1)
 
-ann = LimeExplainer(magpie, dataset,split='test')
-ann.explain_dataset(class_=1)
+# ann = LimeExplainer(magpie, dataset,split='test')
+# ann.explain_dataset(class_=1)
 
-ann = LimeExplainer(synth, dataset,split='test')
+ann = LimeExplainer(synth, dataset,split='train')
 ann.explain_dataset(class_=1)
 
 # visualize feature attributions
